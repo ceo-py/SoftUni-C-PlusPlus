f4627c0ef221929ad13e9ec8b5ea911953cba67b
ceo-py <sfmf@abv.bg>
2024-01-04 03:04:13 +0200

    new solution

---
+++ 08_Pet_Shop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int a, b;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    double result = (a * 2.50) + (b * 4);
+
+    std::cout << result << " lv." << std::endl;
+
+    return 0;
+}
