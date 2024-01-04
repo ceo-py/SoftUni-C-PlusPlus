52736cfa0cf678ad873e513259019424ecceabd7
ceo-py <sfmf@abv.bg>
2024-01-04 13:26:45 +0200

    new solution

---
+++ CPlusPlus-Basics/Conditional Statements - Lab/02_Greater_Number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int a, b;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    if (a > b)
+    {
+        std::cout << a << std::endl;
+    }
+    else
+    {
+        std::cout << b << std::endl;
+    }
+
+    return 0;
+}
