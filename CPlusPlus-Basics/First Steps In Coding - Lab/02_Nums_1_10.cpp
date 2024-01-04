51343e12e69d3170c0c34b77aa9b18cc695ec763
ceo-py <sfmf@abv.bg>
2024-01-04 02:55:57 +0200

    new solution

---
+++ CPlusPlus-Basics/First Steps In Coding - Lab/02_Nums_1_10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::cout << "1" << std::endl;
+    std::cout << "2" << std::endl;
+    std::cout << "3" << std::endl;
+    std::cout << "4" << std::endl;
+    std::cout << "5" << std::endl;
+    std::cout << "6" << std::endl;
+    std::cout << "7" << std::endl;
+    std::cout << "8" << std::endl;
+    std::cout << "9" << std::endl;
+    std::cout << "10" << std::endl;
+    return 0;
+}
