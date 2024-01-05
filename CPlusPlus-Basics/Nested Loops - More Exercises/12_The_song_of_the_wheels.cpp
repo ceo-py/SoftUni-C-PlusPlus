52533370f3ded1e6b32f9ab3296c14b95ff35478
ceo-py <sfmf@abv.bg>
2024-01-05 20:52:19 +0200

    new solution

---
+++ CPlusPlus-Basics/Nested Loops - More Exercises/12_The_song_of_the_wheels.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+    std::cin >> number;
+
+    std::string guessed_passwords = "";
+    int counter = 0;
+
+    for (int a = 1; a < 10; ++a)
+    {
+        for (int b = 1; b < 10; ++b)
+        {
+            for (int c = 1; c < 10; ++c)
+            {
+                for (int d = 1; d < 10; ++d)
+                {
+                    if (a * b + c * d == number && a < b && c > d)
+                    {
+                        std::cout << a << b << c << d << " ";
+                        counter += 1;
+
+                        if (counter == 4)
+                        {
+                            guessed_passwords = std::to_string(a) + std::to_string(b) + std::to_string(c) + std::to_string(d);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (counter >= 4)
+    {
+        std::cout << "\nPassword: " << guessed_passwords << std::endl;
+    }
+    else
+    {
+        std::cout << "\nNo!" << std::endl;
+    }
+
+    return 0;
+}
